d11c27403df54c2095c82aacc29ec11294c8659e
Ted C. Howard <ted@tedchoward.com>
2021-03-28 15:00:30 -0500

    feat: initialize program counter to 0x8000

--- makerom.rb
+++ makerom.rb
@@ -2,9 +2,10 @@
 
 # frozen_string_literal: true
 
-rom = Array.new(32_768, 0xea)
+rom = Array.new(32_768, 0xEA)
 
-puts rom
+rom[0x7FFC] = 0x00 # CPU will "see" this as 0xFFFC
+rom[0x7FFD] = 0x80
 
 File.open('rom.bin', 'wb') do |f|
   rom.each { |byte| f.write(byte.chr) }
