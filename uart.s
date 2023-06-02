c438d0c3d3d0b65043fdf1cbdd1165cd0e5c624b
Ted C. Howard <ted@tedchoward.com>
2023-06-02 14:58:15 -0500

    change baud rate to 19200

--- uart.s
+++ uart.s
@@ -14,7 +14,7 @@
     .export uart_init, put_chr, poll_chr, wait_tick
 
 uart_init:
-    lda #%00011110  ; 1 stop bit; WL=8; baud; 9600
+    lda #%00011111  ; 1 stop bit; WL=8; baud; 19,200
     sta ACIA_CONTROL
     lda #%00001011  ; no parity; echo off; interrupt off; DTR active low
     sta ACIA_COMMAND
@@ -45,11 +45,11 @@ wait_tick:
     pla
     rts
 
-; designed to wait the time it takes to send 10 bits at 9600 baud
-; overhead = 21 cycles, loop = 204 * 5 = 1020 cycles
+; designed to wait the time it takes to send 10 bits at 19200 baud
+; overhead = 21 cycles, loop = 100 * 5 = 500 cycles
 wait_loop:
   phx
-  ldx #204
+  ldx #100
 : dex
   bne :-
   plx
